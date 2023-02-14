556b2e57425d5a547278e1d43e8e59d1e8f16486
global-p0lluxstar <global-web-registra@yandex.ru>
2023-02-14 23:51:03 +0300

    140223-23:50

--- src/index.js
+++ src/index.js
@@ -2,5 +2,28 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+
+  if(typeof matrix != 'object'){ // если приходи пустой аргумент, возвращаем пустой массив
+    return [];
+  };
+
+  matrix.forEach((el, index) => { // четные элементы сортируем по возрастанию, нечетные по убывани 
+    if(index % 2 === 0){
+      el.sort(function(a, b){
+        return a - b;
+      });
+    }else{
+      el.sort(function(a, b){
+        return b - a;
+      });
+    };
+  });
+
+  let resArr = [];
+
+  for(let i = 0; i < matrix.length; i++){ // из двхумерного массива matrix, делаем одномерный массив resArr
+    resArr = resArr.concat(matrix[i]);
+  };
+
+  return resArr;
+};
